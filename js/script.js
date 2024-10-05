440709d928ac431ad413a485236bc94415800d2b
CamillaKleymen <camillakleymen@outlook.com>
2024-10-05 17:57:12 +0500

    almost done

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
 $(document).ready(function() {
-    
-        $('.multiple-items').slick({
-            infinite: true,
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            dots: true,
-            arrows: true,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
                 }
-            ]
-        });
-   
-    
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.prev').on('click', function() {
+        $('.multiple-items').slick('slickPrev');
+    });
+    $('.next').on('click', function() {
+        $('.multiple-items').slick('slickNext');
+    });
 });
