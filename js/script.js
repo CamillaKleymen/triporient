5f50d0f08cc34846a5b8956ab5373206c508c40a
CamillaKleymen <camillakleymen@outlook.com>
2024-10-08 03:44:54 +0500

    footer done + paginator too

--- js/script.js
+++ js/script.js
@@ -1,34 +1,44 @@
 $(document).ready(function() {
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
+    // Функция для инициализации слайдера
+    function initSlider(selector) {
+        $(selector).slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            prevArrow: $(selector).siblings('.paginator').find('.prev'),
+            nextArrow: $(selector).siblings('.paginator').find('.next'),
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
                 }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
+            ]
+        });
 
-    $('.prev').on('click', function() {
-        $('.multiple-items').slick('slickPrev');
-    });
-    $('.next').on('click', function() {
-        $('.multiple-items').slick('slickNext');
-    });
+        // Обработчики событий для кнопок
+        $(selector).siblings('.paginator').find('.prev').on('click', function() {
+            $(selector).slick('slickPrev');
+        });
+        $(selector).siblings('.paginator').find('.next').on('click', function() {
+            $(selector).slick('slickNext');
+        });
+    }
+
+    // Инициализация слайдеров
+    initSlider('.multiple-items');
+    initSlider('.popular-destinations .multiple-items');
+    initSlider('.testimonial .multiple-items');
 });
+
